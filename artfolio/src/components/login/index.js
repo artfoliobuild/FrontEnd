c0fb5cd9f39f475060c88b9d0875d292327abb12
CatsAreMLG <joshuadgon@gmail.com>
2019-02-15 17:56:28 -0600

    Add login/signup component functionality

--- artfolio/src/components/login/index.js
+++ artfolio/src/components/login/index.js
@@ -1,33 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default class Login extends React.Component {
+export default class LogIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: "",
       user: "",
       pass: ""
     };
   }
+  handleChange = e => {
+    this.setState({
+      [e.target.dataset.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addUser({
+      email: this.state.email,
+      user: this.state.user,
+      pass: this.state.pass
+    });
+  };
+  signIn = e => {
+    e.preventDefault();
+  };
   render() {
     return (
       <div className="form_container">
-        <form className="login">
+        <form onSubmit={this.handleSubmit} className="login">
           <h2 className="login_title">Log In</h2>
           <input
             className="login_input"
+            type="email"
+            data-name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            className="login_input"
             type="text"
+            data-name="user"
             placeholder="Username"
             value={this.state.user}
+            onChange={this.handleChange}
+            required
           />
           <input
             className="login_input"
             type="password"
+            data-name="pass"
             placeholder="Password"
             value={this.state.pass}
+            onChange={this.handleChange}
+            required
           />
-          <button className="login_button">Log In</button>
-          <button className="login_signup">
+          <button
+            onSubmit={this.handleSubmit}
+            onClick={this.handleSubmit}
+            className="login_button"
+          >
+            Log In
+          </button>
+          <button
+            onSubmit={this.signIn}
+            onClick={this.signIn}
+            className="login_signup"
+          >
             <Link to="/signup">Sign Up</Link>
           </button>
         </form>
--- artfolio/src/components/signup/index.js
+++ artfolio/src/components/signup/index.js
@@ -5,40 +5,69 @@ export default class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: "",
       user: "",
       pass: ""
     };
   }
-  preventDefault = e => {
+  handleChange = e => {
+    this.setState({
+      [e.target.dataset.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addUser({
+      email: this.state.email,
+      user: this.state.user,
+      pass: this.state.pass
+    });
+  };
+  logIn = e => {
     e.preventDefault();
   };
   render() {
     return (
       <div className="form_container">
-        <form onSubmit={this.preventDefault} className="signup">
+        <form onSubmit={this.handleSubmit} className="signup">
           <h2 className="signup_title">Sign Up</h2>
           <input
             className="signup_input"
+            type="email"
+            data-name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            className="signup_input"
             type="text"
+            data-name="user"
             placeholder="Username"
             value={this.state.user}
+            onChange={this.handleChange}
+            required
           />
           <input
             className="signup_input"
             type="password"
+            data-name="pass"
             placeholder="Password"
             value={this.state.pass}
+            onChange={this.handleChange}
+            required
           />
           <button
-            onSubmit={this.preventDefault}
-            onClick={this.preventDefault}
+            onSubmit={this.handleSubmit}
+            onClick={this.handleSubmit}
             className="signup_button"
           >
             Sign Up
           </button>
           <button
-            onSubmit={this.preventDefault}
-            onClick={this.preventDefault}
+            onSubmit={this.logIn}
+            onClick={this.logIn}
             className="signup_login"
           >
             <Link to="/login">Log In</Link>
