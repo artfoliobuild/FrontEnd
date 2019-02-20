405f345af3bb3ed93103258f9a62a463fe409017
CatsAreMLG <joshuadgon@gmail.com>
2019-02-19 21:05:19 -0600

    Add username to Comment

--- artfolio/src/components/comment/index.js
+++ artfolio/src/components/comment/index.js
@@ -4,7 +4,7 @@ const Comment = props => {
   return (
     <div className="comment">
       <img className="comment_avatar" src={props.comment.avatar} alt="" />
-      <span className="comment_user">user-{props.comment.user_id}</span>
+      <span className="comment_user">{props.comment.username}</span>
       <span className="comment_content">{props.comment.content}</span>
     </div>
   );
