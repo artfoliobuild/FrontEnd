341c6f561c03aad074836124bb8015f2573f71e2
CatsAreMLG <joshuadgon@gmail.com>
2019-02-21 22:11:36 -0600

    Add like functionality

--- artfolio/src/components/mobileModal/index.js
+++ artfolio/src/components/mobileModal/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import axios from "axios";
+import moment from "moment";
+import { css } from "@emotion/core";
+import { ClipLoader } from "react-spinners";
+
 import { FaRegHeart } from "react-icons/fa";
 import { FiShare, FiTrash2 } from "react-icons/fi";
 import { ReactComponent as Edit } from "../../images/icons/edit.svg";
-import moment from "moment";
 
 import Comments from "../comments";
 import Error404 from "../Error404";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const image = require("../../images/profile/42696764_388532598354278_2259473674702684160_n.jpg");
 const BACKEND = process.env.REACT_APP_BACKEND.replace(/"/g, "");
@@ -20,7 +29,9 @@ export default class MobileModal extends React.Component {
       edit: "",
       editing: false,
       deleting: false,
-      err: null
+      err: null,
+      loading: true,
+      liked: false
     };
   }
   componentDidMount() {
@@ -114,6 +125,30 @@ export default class MobileModal extends React.Component {
         );
     } else this.props.history.push("/login");
   };
+  handleLike = _ => {
+    if (this.checkLoad())
+      axios
+        .put(BACKEND + "/posts/" + this.state.post.id, {
+          description: this.state.post.description,
+          likes: this.state.post.likes + 1,
+          image: this.state.post.image,
+          token: this.props.user
+        })
+        .then(res => {
+          let newPost = this.state.post;
+          newPost.likes += 1;
+          this.setState({
+            post: newPost,
+            liked: true
+          });
+        })
+        .catch(err =>
+          this.setState({
+            err: <div>There was an issue liking the post</div>
+          })
+        );
+    else this.props.history.push("/login");
+  };
   editPost = _ =>
     this.setState(prevState => ({
       edit: this.state.post.description,
@@ -155,73 +190,93 @@ export default class MobileModal extends React.Component {
               this.state.editing || this.state.deleting ? this.close : null
             }
           >
-            <div className="mobile_modal_artist">
-              <img
-                className="mobile_modal_artist_pic"
-                src={image}
-                alt="artist"
+            {this.state.loading ? (
+              <ClipLoader
+                css={override}
+                sizeUnit={"px"}
+                size={150}
+                color={"#123abc"}
+                loading={this.state.loading}
               />
-              {this.props.artist}
-            </div>
-            <div className="mobile_modal_image_container">
-              <img
-                className="mobile_modal_image"
-                src={this.state.post ? this.state.post.image : this.props.src}
-                alt="car"
-              />
-            </div>
-            <div className="mobile_modal_post">
-              <div className="mobile_modal_icons">
-                <FaRegHeart size="24px" />
-                <FiShare size="24px" />
-                {this.props.verifyUser(this.props.user) ? (
-                  this.props.verifyUser(this.props.user).admin ? (
-                    <span className="mobile_modal_icons_right">
-                      <Edit
-                        className="mobile_modal_icons_icon"
+            ) : (
+              <>
+                <div className="mobile_modal_artist">
+                  <img
+                    className="mobile_modal_artist_pic"
+                    src={image}
+                    alt="artist"
+                  />
+                  {this.props.artist}
+                </div>
+                <div className="mobile_modal_image_container">
+                  <img
+                    className="mobile_modal_image"
+                    src={
+                      this.state.post ? this.state.post.image : this.props.src
+                    }
+                    alt="car"
+                  />
+                </div>
+                <div className="mobile_modal_post">
+                  <div className="mobile_modal_icons">
+                    <span onClick={this.handleLike}>
+                      <FaRegHeart
+                        className="modal_icons_icon"
+                        fill={this.state.liked ? "red" : "black"}
                         size="24px"
-                        onClick={this.editPost}
-                      />
-                      <FiTrash2
-                        className="mobile_modal_icons_icon"
-                        size="24px"
-                        onClick={this.deletePost}
                       />
                     </span>
-                  ) : null
-                ) : null}
-              </div>
-              <div className="mobile_modal_likes">
-                {this.state.posts ? this.state.posts.likes : 0} likes
-              </div>
-              <div className="mobile_modal_date">
-                {this.state.post
-                  ? moment(this.state.post.created_at).fromNow()
-                  : null}
-              </div>
-              <div className="mobile_modal_comments">
-                {this.state.post ? (
-                  <span>
-                    <b>{this.props.artist}</b> {this.state.post.description}
-                  </span>
-                ) : null}
-                <Comments
-                  comments={this.state.comments}
-                  user={this.props.user}
-                  verifyUser={this.props.verifyUser}
-                  refreshPost={this.refreshPost}
-                />
-              </div>
-            </div>
-            <form onSubmit={this.handleMessage}>
-              <input
-                type="text"
-                value={this.state.comment}
-                data-name="comment"
-                placeholder="Add a comment..."
-                onChange={this.handleChange}
-              />
-            </form>
+                    {/* <FiShare size="24px" /> */}
+                    {this.props.verifyUser(this.props.user) ? (
+                      this.props.verifyUser(this.props.user).admin ? (
+                        <span className="mobile_modal_icons_right">
+                          <Edit
+                            className="mobile_modal_icons_icon"
+                            size="24px"
+                            onClick={this.editPost}
+                          />
+                          <FiTrash2
+                            className="mobile_modal_icons_icon"
+                            size="24px"
+                            onClick={this.deletePost}
+                          />
+                        </span>
+                      ) : null
+                    ) : null}
+                  </div>
+                  <div className="mobile_modal_likes">
+                    {this.state.post ? this.state.post.likes : 0} likes
+                  </div>
+                  <div className="mobile_modal_date">
+                    {this.state.post
+                      ? moment(this.state.post.created_at).fromNow()
+                      : null}
+                  </div>
+                  <div className="mobile_modal_comments">
+                    {this.state.post ? (
+                      <span>
+                        <b>{this.props.artist}</b> {this.state.post.description}
+                      </span>
+                    ) : null}
+                    <Comments
+                      comments={this.state.comments}
+                      user={this.props.user}
+                      verifyUser={this.props.verifyUser}
+                      refreshPost={this.refreshPost}
+                    />
+                  </div>
+                </div>
+                <form onSubmit={this.handleMessage}>
+                  <input
+                    type="text"
+                    value={this.state.comment}
+                    data-name="comment"
+                    placeholder="Add a comment..."
+                    onChange={this.handleChange}
+                  />
+                </form>
+              </>
+            )}
           </div>
           {this.state.editing ? (
             <form className="mobile_modal_edit" onSubmit={this.handleEdit}>
